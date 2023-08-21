2dc47268baff30bf8123cbe9612342ef445fedae
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-21 11:31:33 +0100

    feat: math.js file created and set up as default and named exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import myLocation, { message, name, getGreeting } from "./myModule";
+import add, { subtract } from "./math";
 
+//testing myModule.js imports
 console.log(message);
 console.log(name);
 console.log(myLocation);
 console.log(getGreeting("jessica"));
+// testing math.js imports
+console.log(add(3, 5));
+console.log(subtract(5, 9));
