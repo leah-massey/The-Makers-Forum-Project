992e1762c5f39f29074f24502688267174a39112
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-23 10:24:36 +0100

    refactor: using spread operator

--- src/index.js
+++ src/index.js
@@ -160,9 +160,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age,
+        ...args,
       };
 
       users.push(user);
@@ -180,10 +178,7 @@ const resolvers = {
 
       const newPost = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author,
+        ...args,
       };
 
       posts.push(newPost);
@@ -207,9 +202,7 @@ const resolvers = {
 
       const newComment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post,
+        ...args,
       };
 
       comments.push(newComment);
